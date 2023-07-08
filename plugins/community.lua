a2b1aed3f53b59d2981c22405e325be23844a9ae
adgor <adgor000@gmail.com>
2023-07-08 18:59:30 +0200

    updt community

--- plugins/community.lua
+++ plugins/community.lua
@@ -14,6 +14,7 @@ return {
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.prisma" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.editing-support.treej" },
 }
