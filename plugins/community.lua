aed322d513dab3a492ebf73b8e8ee671148a5d8d
adgor <adgor000@gmail.com>
2023-07-18 11:08:17 +0200

    commenting comunnity treej

--- plugins/community.lua
+++ plugins/community.lua
@@ -16,5 +16,5 @@ return {
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.prisma" },
   { import = "astrocommunity.motion.nvim-surround" },
-  { import = "astrocommunity.editing-support.treej" },
+  -- { import = "astrocommunity.editing-support.treej" },
 }
